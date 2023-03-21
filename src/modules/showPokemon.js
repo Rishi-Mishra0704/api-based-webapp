e06433e25015bae250974fe982e797574dd3505a
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-03-21 14:36:47 +0530

    Add like button counter and comment button

--- src/modules/showPokemon.js
+++ src/modules/showPokemon.js
@@ -1,30 +1,64 @@
-const BASE_URL = 'https://pokeapi.co/api/v2/pokemon?limit=6&offset=0';
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=6&offset=0";
+const INVOLVE_API =
+  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/eNcbFL1NPb8wUFbHRoP3/likes";
 async function showPokemon() {
   const response = await fetch(BASE_URL);
   const data = await response.json();
 
-  const likeableItems = document.getElementById('likeable-items');
+  const likeableItems = document.getElementById("likeable-items");
 
-  data.results.forEach(async pokemon => {
-    const listItem = document.createElement('li');
-    const pokemonData = await fetch(pokemon.url).then(res => res.json());
+  const promises = data.results.map((pokemon) => {
+    return fetch(pokemon.url).then((res) => res.json());
+  });
 
-    // Get abilities and stats
-    const abilities = pokemonData.abilities.map(ability => ability.ability.name).join(', ');
-    const stats = pokemonData.stats.map(stat => `${stat.stat.name}: ${stat.base_stat}`).join(', ');
+  const pokemonDataArray = await Promise.all(promises);
 
-    listItem.innerHTML = `
+  pokemonDataArray.forEach((pokemonData) => {
+    const listItem = createListItem(pokemonData);
+    likeableItems.appendChild(listItem);
+  });
+}
+
+function createListItem(pokemonData) {
+  const listItem = document.createElement("li");
+
+  const abilities = pokemonData.abilities
+    .map((ability) => ability.ability.name)
+    .join(", ");
+  const stats = pokemonData.stats
+    .map((stat) => `${stat.stat.name}: ${stat.base_stat}`)
+    .join(", ");
+
+  listItem.innerHTML = `
       <img class="item-image" src="${pokemonData.sprites.front_default}">
-      <span class="item-name">${pokemon.name}</span>
+      <span class="item-name">${pokemonData.name}</span>
       <span class="item-name">${pokemonData.weight}</span>
       <span class="item-name">Abilities: ${abilities}</span>
       <span class="item-name">Stats: ${stats}</span>
+      <button class = "comments">Comment</button>
       <button class="like-button">
         Like <span class="badge">0</span>
       </button>
     `;
 
-    likeableItems.appendChild(listItem);
+  const likeButton = listItem.querySelector(".like-button");
+  const likeCount = listItem.querySelector(".badge");
+
+  likeButton.addEventListener("click", async () => {
+    const response = await fetch(INVOLVE_API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        item_id: pokemonData.id,
+      }),
+    });
+    const likes = await response.json();
+    likeCount.innerHTML = likes.likes;
   });
+
+  return listItem;
 }
+
 export default showPokemon;
